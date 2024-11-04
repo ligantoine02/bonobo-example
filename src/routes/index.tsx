b0b35449d03e5c22f40c17f5069246de01b13123
Glorieux Lukama <ir.antoineglorieux@gmail.com>
2024-11-04 09:32:08 +0200

    fix: add bonoboui as dev dependency

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,30 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { Card, Heading } from "bonoboui-qwik";
 
 export default component$(() => {
-  return (
-    <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
-    </>
-  );
+	return (
+		<>
+			<h1>Hi 👋</h1>
+			<div>
+				Can't wait to see what you build with qwik!
+				<br />
+				Happy coding.
+			</div>
+
+			<Card horizontal>
+				<Heading>Trying BonoboUI, my first npm package</Heading>
+			</Card>
+		</>
+	);
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
+	title: "Welcome to Qwik",
+	meta: [
+		{
+			name: "description",
+			content: "Qwik site description",
+		},
+	],
 };
